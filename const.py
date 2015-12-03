0517a29af7945fafae432ce4512b0c34f8d7eeed
Quentin Stoeckel <chteuchteu@gmail.com>
2015-12-03 13:40:59 +0100

    Fix boolean case

--- const.py
+++ const.py
@@ -5,4 +5,4 @@ GCM_PROXY_URL = 'http://gcm-proxy.munin-for-android.com/trigger/declareAlert'
 GCM_PROXY_TEST_URL = 'http://gcm-proxy.munin-for-android.com/trigger/test'
 
 # If true, the server will temporary store the XML structure for debugging purposes if it failed parsing it _only_
-HELP_DIAGNOSE = false
+HELP_DIAGNOSE = False
